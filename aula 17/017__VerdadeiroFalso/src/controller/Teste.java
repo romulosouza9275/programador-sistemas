04d55dd42e4f02ba0083a9cc770185afdf3c1a41
romulosouza9275 <44073235+romulosouza9275@users.noreply.github.com>
2018-11-01 20:47:44 -0300

    Add files via upload

---
+++ aula 17/017__VerdadeiroFalso/src/controller/Teste.java
@@ -0,0 +1,18 @@
+package controller;
+
+public class Teste {
+	public static void main(String[] args) {
+	boolean b =false;
+	boolean a = true;
+	boolean c = a || b;
+	if( a != b) {
+	System.out.println("iguais");
+	}
+	else
+	{
+		System.out.println("Diferentes");
+	}
+	}}
+
+
+	
