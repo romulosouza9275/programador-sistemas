442477b09495180afe19ec9ee24c0d50514718e9
romulosouza9275 <44073235+romulosouza9275@users.noreply.github.com>
2018-11-09 21:34:26 -0200

    Add files via upload

---
+++ ExemploFormatData.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+
+Date data = new Date();
+System.out.println("Data"+ data);
+
+@Deprecated
+Date dataInserida = new date(2011,9,14);
+System.out.println9=("Data Inserida"+dataInserida);
+//Esta Classe está obsoleta
+
+Calendar data2 + new
+
+}
