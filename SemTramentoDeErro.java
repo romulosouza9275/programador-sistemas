fe4319a59eb396eae7a3a7cceba1591a7e4b1bee
romulosouza9275 <44073235+romulosouza9275@users.noreply.github.com>
2018-11-12 21:39:58 -0200

    Add files via upload

---
+++ SemTramentoDeErro.java
@@ -0,0 +1,14 @@
+package controller;
+import util.teclado;
+public class SemTramentoDeErro {
+
+public static void main(String[] args) {
+
+double a =Teclado.LerDouble("digite Numerador: ");
+double b =Teclado.lerDouble("Digite o Denominador: ");
+
+double c = a/b;
+System.out.println("Resultado da divisão" de a por b:" + c);"
+		
+}
+}
