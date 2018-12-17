a524b36e1cfd5d6832ea82670d518a671d8faca5
romulosouza9275 <44073235+romulosouza9275@users.noreply.github.com>
2018-12-17 18:36:48 -0200

    Add files via upload

---
+++ Aula15/src/model/SuperClasseContaCorrente.java
@@ -0,0 +1,39 @@
+package model;
+
+public class ContaCorrente {
+       private String titular;
+       private String agencia;
+       private String numeroConta;
+       private double saldo;
+
+public String getTitular() {
+	return titular;
+}
+public void set titular(String titular) {
+	this.titular = titular;
+}
+public void setAgencia() {
+	return agencia;	
+}
+public String getNumeroConta() {
+}
+	return numeroConta;
+}
+public void setNumeroConta(String numeroConta) {
+	this.numeroConta = numeroConta;	
+}
+public double get.Saldo() {
+	return saldo;
+}
+public void setSaldo(double) {
+      this.saldo = saldo;
+}
+public void setSaldo(double saldo) {
+	 this.saldo = saldo;
+}
+public ContaCorrente() {
+	super();
+}
+public ContaCorrente(String titular,String agencia,String numeroconta,
+		double saldo) {	
+}
