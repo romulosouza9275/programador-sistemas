7593d08a66e0e8ff4082d8851774adf989180034
romulosouza9275 <44073235+romulosouza9275@users.noreply.github.com>
2018-12-18 21:12:46 -0200

    Add files via upload

---
+++ InfoNote_08/src/controller/InfoNote_08.java
@@ -0,0 +1,25 @@
+package controller;
+
+public class InfoNote_08 {
+	int tipo;
+	private String login;
+	private String senha;
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
